fc0fcc9f393a4aee2167d13369b95a40d9c5bddd
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-08-02 08:43:52 +0530

    Create Problem118.java

---
+++ LeetCode/Problem118.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int i=0 ; i<numRows ; i++){
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            if(i > 0){
+                List<Integer> prevRow = res.get(i-1);
+                for(int j=1; j<i; j++){
+                    row.add(prevRow.get(j-1)+prevRow.get(j));
+                }
+                row.add(1);
+            }
+
+            res.add(row);
+        }
+
+        return res;
+    }
+}
